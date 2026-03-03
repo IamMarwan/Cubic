f080bf95d9564a200c2f57d0a74fb669873aaf5e
Marwan <marwan.dahir@yahoo.com>
2026-03-03 23:30:08 +0400

    Week 06 - Production AI Knowledge Platform Complete

---
+++ Week_06/app/config.py
@@ -0,0 +1,9 @@
+﻿import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+VECTOR_DB_PATH = "storage/vectordb"
+UPLOAD_FOLDER = "storage/uploads"
+
+EMBEDDING_MODEL = "text-embedding-3-small"
+LLM_MODEL = "gpt-4o-mini"
---
+++ Week_06/app/schemas.py
@@ -0,0 +1,11 @@
+﻿from pydantic import BaseModel
+
+
+class QuestionRequest(BaseModel):
+    project_name: str
+    question: str
+
+
+class WebsiteRequest(BaseModel):
+    project_name: str
+    url: str
